47c8f66c9b1d3e247600370c87d8cbd9a7f1fa13
Noolii314 <86788401+Noolii314@users.noreply.github.com>
2021-07-26 16:29:10 +0200

    Add files via upload

---
+++ D-W-T.py
@@ -0,0 +1,27 @@
+#Write a Python program to calculate a dog's age in dog's years
+md=float(input('mosha e qenit='))
+if md<=2:
+    mdn=md*10.5
+    print('mosha e qenit sipas viteve te njeriut eshte ',mdn)
+elif md>2:
+    mdn=((md-2)*4)+21
+    print('mosha e qenit sipas viteve te njeriut eshte ',mdn)
+# Write a Python program to check whether an alphabet is a vowel or consonant
+bashke= [ 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n','p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z']
+zanore=['a','e','i','o','u','y']
+a=input('shkronja e deshiruaar:')
+if a in bashke:
+    print(f'{a} eshte bashke')
+elif a in zanore:
+    print(a,'eshte zanore')
+#Write a Python program to check a triangle is equilateral, isosceles or scalene
+x=int(input('x='))
+y=int(input('y='))
+z=int(input('z='))
+if x==y==z:
+    print('trekendesh barabrinjesh')
+elif x==y or y==z or x==z:
+    print('trekendesh barakrahesh')
+else:
+    print('treken)
+
