c72f0aad481b4e8f342c616e850310c14dd02104
Noolii314 <86788401+Noolii314@users.noreply.github.com>
2021-07-28 13:55:57 +0200

    Add files via upload

---
+++ pangram.py
@@ -0,0 +1,17 @@
+#Write a Python function to check whether a string is a pangram or not.
+
+a=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+b="The quick brown fox jumps over the lAzy dokdlkslg"
+s=b.lower()
+c=[]
+for i in a:
+    if i==' ':
+        pass
+    elif i in s:
+        c.append(i)
+if len(c)==len(a):
+    print('b is panagram')
+else:
+    print("b isn't pangram")
+
+
