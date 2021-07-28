6126d4c504ec093e57a4233f6254b9739c179aea
Noolii314 <86788401+Noolii314@users.noreply.github.com>
2021-07-28 21:34:37 +0200

    Update pangram.py

--- pangram.py
+++ pangram.py
@@ -1,5 +1,5 @@
-#Write a Python function to check whether a string is a pangram or not.
-
+#Q/Write a Python function to check whether a string is a pangram or not.
+#A/
 a=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 b="The quick brown fox jumps over the lAzy dokdlkslg"
 s=b.lower()
