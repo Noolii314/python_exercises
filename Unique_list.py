e8122722a6646f1b35ca07467b1de4df6a16da8a
Noolii314 <86788401+Noolii314@users.noreply.github.com>
2021-08-16 18:19:55 +0200

    Update Unique_list.py

--- Unique_list.py
+++ Unique_list.py
@@ -8,6 +8,20 @@ for i in range(len(a)-1):
 b.append(a[len(a)-1])
 print(b)
 
+#Write a Python function that takes a sequence of numbers and determines whether all the numbers are different from each other.
+a=[2,6,8,5,9]
+def allUniqu(a):
+    b = []
+    for i in a:
+        for j in a:
+            if i == j:
+                b.append(i)
+    if len(a) == len(b):
+        return True
+    else:
+        return False
+print(allUniqu(a))
+
 
 
 
